8fb4cb56ad830c0426a848a23862855520febf02
Ian hu <h878688736@gmail.com>
2023-09-07 18:04:15 -0700

    finish cal

--- module2/src/main/java/com/itheima/module2/CalController.java
+++ module2/src/main/java/com/itheima/module2/CalController.java
@@ -8,31 +8,40 @@ import java.text.NumberFormat;
 
 @Controller
 public class CalController {
+
+    static void check(double p, int m, double yr, int type) {
+        if (p <= 0) {
+            throw new IllegalArgumentException("capital should more than 0");
+        }
+        if (m < 1 || m > 360) {
+            throw new IllegalArgumentException("month should between 1 and 360");
+        }
+        if (yr < 1.0 || yr > 36.0) {
+            throw new IllegalArgumentException("interest rate/year should between 1 and 36");
+        }
+        if (type !=0 && type != 1) {
+            throw new IllegalArgumentException("error");
+        }
+    }
+
+    static String[] cal0(double p, int m, double yr) {
+        double mr = yr / 12.0 / 100;
+        double pow = Math.pow(1 + mr, m);
+        double payment = p * mr * pow / (pow - 1);
+        return new String[]{
+                NumberFormat.getCurrencyInstance().format(payment * m),
+                NumberFormat.getCurrencyInstance().format(payment * m - p)
+        };
+    }
+
     @RequestMapping("/cal")
     @ResponseBody
     String[] cal(double yr, double p, int m, int type) {
+        check(p, m, yr, type);
         if (type == 0) {
-            double mr = yr / 12.0 / 100;
-            double pow = Math.pow(1 + mr, m);
-            double payment = p * mr * pow / (pow - 1);
-            return new String[]{
-                    NumberFormat.getCurrencyInstance().format(payment * m),
-                    NumberFormat.getCurrencyInstance().format(payment * m - p)
-            };
+            return cal0(p, m, yr);
         } else {
-            double payPrincipal = p / m;
-            double backup = p;
-            double mr = yr / 12.0 / 100;
-            double payInterestTotal = 0.0;
-            for (int i = 0; i < m; i++) {
-                double payInterest = p * mr;
-                p -= payPrincipal;
-                payInterestTotal += payInterest;
-            }
-            return new String [] {
-                    NumberFormat.getCurrencyInstance().format(backup + payInterestTotal),
-                    NumberFormat.getCurrencyInstance().format(payInterestTotal)
-            };
+            return cal1(yr, p, m);
         }
 
 
@@ -40,47 +49,72 @@ public class CalController {
 
     }
 
+    private static String[] cal1(double yr, double p, int m) {
+        double payPrincipal = p / m;
+        double backup = p;
+        double mr = yr / 12.0 / 100;
+        double payInterestTotal = 0.0;
+        for (int i = 0; i < m; i++) {
+            double payInterest = p * mr;
+            p -= payPrincipal;
+            payInterestTotal += payInterest;
+        }
+        return new String[]{
+                NumberFormat.getCurrencyInstance().format(backup + payInterestTotal),
+                NumberFormat.getCurrencyInstance().format(payInterestTotal)
+        };
+    }
+
     @RequestMapping("/details")
     @ResponseBody
     String[][] details(double yr, double p, int m, int type) {
+        check(p, m, yr, type);
         if (type == 0) {
-            String[][] a2 = new String[m][];
-
-            double mr = yr / 12.0 / 100;
-            double pow = Math.pow(1 + mr, m);
-            double payment = p * mr * pow / (pow - 1);
-            for (int i = 0; i < m; i++) {
-                double payInterest = p * mr;
-                double payPrincipal = payment - payInterest;
-                p -= payPrincipal;
-                String[] row = new String[]{
-                        (i + 1) + "",
-                        NumberFormat.getCurrencyInstance().format(payment),
-                        NumberFormat.getCurrencyInstance().format(payPrincipal),
-                        NumberFormat.getCurrencyInstance().format(payInterest),
-                        NumberFormat.getCurrencyInstance().format(p)
-                };
-                a2[i] = row;
-            }
-            return a2;
+            return details0(yr, p, m);
         } else {
-            String[][] a2 = new String[m][];
-            double payPrincipal = p / m;
-            double mr = yr / 12.0 / 100;
-            for (int i = 0; i < m; i++) {
-                double payInterest = p * mr;
-                p -= payPrincipal;
-                double payment = payPrincipal + payInterest;
-                String[] row = new String[] {
-                        (i + 1) + "",
-                        NumberFormat.getCurrencyInstance().format(payment),
-                        NumberFormat.getCurrencyInstance().format(payPrincipal),
-                        NumberFormat.getCurrencyInstance().format(payInterest),
-                        NumberFormat.getCurrencyInstance().format(p)
-                };
-                a2[i] = row;
-            }
-            return a2;
+            return details1(yr, p, m);
         }
+    }
+
+    private static String[][] details1(double yr, double p, int m) {
+        String[][] a2 = new String[m][];
+        double payPrincipal = p / m;
+        double mr = yr / 12.0 / 100;
+        for (int i = 0; i < m; i++) {
+            double payInterest = p * mr;
+            p -= payPrincipal;
+            double payment = payPrincipal + payInterest;
+            String[] row = new String[] {
+                    (i + 1) + "",
+                    NumberFormat.getCurrencyInstance().format(payment),
+                    NumberFormat.getCurrencyInstance().format(payPrincipal),
+                    NumberFormat.getCurrencyInstance().format(payInterest),
+                    NumberFormat.getCurrencyInstance().format(p)
+            };
+            a2[i] = row;
+        }
+        return a2;
+    }
+
+    private static String[][] details0(double yr, double p, int m) {
+        String[][] a2 = new String[m][];
+
+        double mr = yr / 12.0 / 100;
+        double pow = Math.pow(1 + mr, m);
+        double payment = p * mr * pow / (pow - 1);
+        for (int i = 0; i < m; i++) {
+            double payInterest = p * mr;
+            double payPrincipal = payment - payInterest;
+            p -= payPrincipal;
+            String[] row = new String[]{
+                    (i + 1) + "",
+                    NumberFormat.getCurrencyInstance().format(payment),
+                    NumberFormat.getCurrencyInstance().format(payPrincipal),
+                    NumberFormat.getCurrencyInstance().format(payInterest),
+                    NumberFormat.getCurrencyInstance().format(p)
+            };
+            a2[i] = row;
+        }
+        return a2;
     }
 }
